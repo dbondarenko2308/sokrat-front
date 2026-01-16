e114e5b5d5725f0cdc42fc456df65d29eae744d9
Dmitriy <wiggleeeeee@mail.ru>
2026-01-16 09:56:21 +0300

    build

---
+++ build/js/common.js
@@ -0,0 +1,736 @@
+$(document).ready(function () {
+$(document).ready(function() {
+	const showcase = new Swiper('.showcase__container', {
+		slidesPerView: 1,
+		loop: true,
+
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		}
+	})
+
+	$('.pritok__grid').hover(
+		function() {
+			$('.pritok__grid').addClass('compressed')
+			$(this).addClass('expanded')
+		},
+		function() {
+			$('.pritok__grid').removeClass('compressed expanded')
+		}
+	)
+
+	var init1 = false
+	var swiper1
+	function swiperHow() {
+		if (window.innerWidth < 1500) {
+			if (!init1) {
+				init1 = true
+				swiper1 = new Swiper('.pritok__slider', {
+					slidesPerView: 1,
+					spaceBetween: 10,
+					loop: true,
+					pagination: {
+						el: '.swiper-pagination',
+						type: 'bullets',
+						clickable: true
+					},
+
+					breakpoints: {
+						992: {
+							slidesPerView: 3
+						}
+					}
+				})
+			}
+		} else if (init1) {
+			swiper1.destroy()
+			init1 = false
+		}
+	}
+
+	swiperHow()
+	window.addEventListener('resize', swiperHow)
+
+	const newsmain = new Swiper('.main-news__slider', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+
+		navigation: {
+			nextEl: '.main-news__next',
+			prevEl: '.main-news__prev'
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+		breakpoints: {
+			767: {
+				slidesPerView: 2
+			}
+		}
+	})
+
+	$('.mask').each((i, element) => {
+		const mask = IMask(element, { mask: '+{7} (000) 000-00-00' })
+	})
+
+	$('.footer__nav--label').on('click', function() {
+		$(this).toggleClass('active')
+		$(this).next().toggleClass('active')
+	})
+
+	$(function() {
+		function initMenu() {
+			if (window.innerWidth > 991) {
+				$('.menu__item.dropdown')
+					.off('click')
+					.off('mouseenter mouseleave')
+					.hover(
+						function() {
+							$(this).addClass('is-open')
+						},
+						function() {
+							$(this).removeClass('is-open')
+						}
+					)
+			} else {
+				$('.menu__item.dropdown')
+					.off('mouseenter mouseleave')
+					.off('click')
+					.on('click', function(e) {
+						e.preventDefault()
+						$(this).toggleClass('is-open')
+					})
+			}
+		}
+
+		initMenu()
+		$(window).on('resize', initMenu)
+	})
+
+	$('.header__catalog--top').on('click', function() {
+		$(this).toggleClass('active')
+		$('.header-catalog').toggleClass('active')
+		$('.header').toggleClass('fixed')
+	})
+
+	$('.search-country .header-search__container input').on('input', function() {
+		const query = $(this).val().toLowerCase()
+
+		$('.search-country__nav li').each(function() {
+			const text = $(this).text().toLowerCase()
+			if (text.includes(query)) {
+				$(this).show()
+			} else {
+				$(this).hide()
+			}
+		})
+	})
+
+	let lastScroll = 0
+	const $header = $('.header')
+
+	$(window).on('scroll', function() {
+		let currentScroll = $(this).scrollTop()
+
+		if (currentScroll > lastScroll && currentScroll > 50) {
+			$header.removeClass('scroll')
+		} else if (currentScroll < lastScroll && currentScroll > 50) {
+			$header.addClass('scroll')
+		}
+
+		if (currentScroll <= 50) {
+			$header.removeClass('scroll')
+		}
+
+		lastScroll = currentScroll
+	})
+
+	$('.search-country').each(function() {
+		const $block = $(this)
+		const $input = $block.find('.header-search__container input')
+		const $container = $block.find('.header-search__container')
+		const $clear = $block.find('.header-search__clear')
+		const $listItems = $block.find('.search-country__nav li')
+
+		$input.on('keyup', function() {
+			const query = $(this).val().toLowerCase()
+
+			$container.toggleClass('has-value', query.length > 0)
+
+			$listItems.each(function() {
+				const text = $(this).text().toLowerCase()
+				$(this).toggle(text.includes(query))
+			})
+		})
+
+		$clear.on('click', function() {
+			$input.val('').trigger('keyup')
+			$input.focus()
+		})
+	})
+
+	$('.header').each(function() {
+		const $block = $(this)
+		const $input = $block.find('.header-search__container input')
+		const $container = $block.find('.header-search__container')
+		const $clear = $block.find('.header-search__clear')
+		const $searchContent = $block.find('.header-search-content')
+		const $searchWrap = $block.find('.header-search')
+
+		function updateClasses() {
+			const query = $input.val().toLowerCase()
+			const hasValue = query.length > 0
+
+			$container.toggleClass('has-value', hasValue)
+			$searchContent.toggleClass('active', hasValue)
+			$searchWrap.toggleClass('border', hasValue)
+		}
+
+		$input.on('keyup', updateClasses)
+
+		updateClasses()
+
+		$clear.on('click', function() {
+			$input.val('').trigger('keyup')
+			$input.focus()
+		})
+	})
+
+	$('.header__burger').on('click', function() {
+		$(this).toggleClass('active')
+		$('.header-mobile').toggleClass('active')
+		$('body').toggleClass('menu')
+
+		const svg = $(this).find('svg')
+
+		if ($(this).hasClass('active')) {
+			svg.html(`
+  <path d="M15 15L5 5M15 5L5 15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    `)
+		} else {
+			svg.html(`
+  <path d="M4.16797 14.1667H15.8346M4.16797 10H15.8346M4.16797 5.83334H15.8346" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    `)
+		}
+	})
+
+	$('.header-top__country').on('click', function() {
+		$('.search-country').addClass('active')
+		$('body').addClass('hidden')
+		$('.overhidden').addClass('active')
+
+		$('.header-mobile').removeClass('active')
+		const burger = $('.header__burger')
+		burger.removeClass('active')
+		$('body').removeClass('menu')
+
+		burger.find('svg').html(`
+    <path d="M4.16797 14.1667H15.8346M4.16797 10H15.8346M4.16797 5.83334H15.8346" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  `)
+	})
+
+	$('.search-country__close').on('click', function() {
+		$('.search-country').removeClass('active')
+		$('body').removeClass('hidden')
+		$('.overhidden').removeClass('active')
+	})
+
+	$('.header-search').on('click', function() {
+		$(this).addClass('active')
+		$('.overhidden').addClass('active')
+		$('.header-mobile').removeClass('active')
+		const burger = $('.header__burger')
+		burger.removeClass('active')
+		$('body').removeClass('menu')
+
+		burger.find('svg').html(`
+    <path d="M4.16797 14.1667H15.8346M4.16797 10H15.8346M4.16797 5.83334H15.8346" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  `)
+	})
+
+	$('.overhidden').on('click', function() {
+		$('.search-country').removeClass('active')
+		$('body').removeClass('hidden')
+		$('.overhidden').removeClass('active')
+		$('.header-search-content').removeClass('active')
+		$('.header-search').removeClass('active')
+	})
+
+	$.fancybox.defaults.touch = false
+	$.fancybox.defaults.closeExisting = true
+
+	const $button = $('.down-up')
+	const bottomOffset = 20
+
+	$button.on('click', function() {
+		$('html, body').animate({ scrollTop: 0 }, 600)
+	})
+
+	$(window).on('scroll resize', function() {
+		const scrollTop = $(window).scrollTop()
+		const windowHeight = $(window).height()
+		const windowWidth = $(window).width()
+		let footerTop
+
+		if (windowWidth < 991) {
+			footerTop = $('.footer').offset().top - 160
+		} else {
+			footerTop = $('.footer').offset().top - 160
+		}
+
+		if (scrollTop > 200) {
+			$button.addClass('show')
+		} else {
+			$button.removeClass('show')
+		}
+
+		if (windowWidth < 991) {
+			const stopPoint = footerTop - windowHeight + bottomOffset + 300
+			if (scrollTop > stopPoint) {
+				$button.addClass('active').css({
+					top: footerTop + 220 + 'px',
+					bottom: 'auto'
+				})
+			} else {
+				$button.removeClass('active').css({
+					top: 'auto',
+					bottom: '55px'
+				})
+			}
+		}
+	})
+
+	if ($('.filter__price--slider').length) {
+		$('.filter__price--slider').each(function() {
+			let $range = $(this)
+			let parent = $(this).parent()
+			let $inputFrom = parent.find('.filter__price--low')
+			let $inputTo = parent.find('.filter__price--high')
+			let instance
+			let min = $inputFrom.data('min')
+			let max = $inputTo.data('max')
+			let from = $range.data('from')
+			let to = $range.data('to')
+
+			$range.ionRangeSlider({
+				skin: 'round',
+				type: 'double',
+				onStart: updateInputs,
+				onChange: updateInputs,
+				onFinish: updateInputsWT
+			})
+
+			instance = $range.data('ionRangeSlider')
+
+			function updateInputs(data) {
+				from = data.from
+				to = data.to
+
+				if (data.from === data.min || data.from === '') {
+					$inputFrom.prop('value', '')
+					$inputFrom.prop('placeholder', data.min)
+				} else {
+					$inputFrom.prop('value', from)
+					$inputFrom.prop('placeholder', '')
+				}
+
+				if (data.to === data.max || data.to === '') {
+					$inputTo.prop('value', '')
+					$inputTo.prop('placeholder', data.max)
+				} else {
+					$inputTo.prop('value', to)
+					$inputTo.prop('placeholder', '')
+				}
+			}
+
+			function updateInputsWT(data) {
+				from = data.from
+				to = data.to
+
+				if (data.from === data.min || data.from === '') {
+					$inputFrom.prop('value', '')
+					$inputFrom.prop('placeholder', data.min)
+				} else {
+					$inputFrom.prop('value', from)
+					$inputFrom.prop('placeholder', '')
+				}
+
+				if (data.to === data.max || data.to === '') {
+					$inputTo.prop('value', '')
+					$inputTo.prop('placeholder', data.max)
+				} else {
+					$inputTo.prop('value', to)
+					$inputTo.prop('placeholder', '')
+				}
+
+				$inputFrom.trigger('keyup')
+				$inputTo.trigger('keyup')
+			}
+
+			$inputFrom.on('change', function() {
+				var val = $(this).prop('value')
+				if (val < min) {
+					val = min
+				} else if (val > to) {
+					val = to
+				}
+
+				instance.update({
+					from: val
+				})
+				$(this).prop('value', val)
+			})
+
+			$inputTo.on('change', function() {
+				var val = $(this).prop('value')
+				if (val < from) {
+					val = from
+				} else if (val > max) {
+					val = max
+				}
+				instance.update({
+					to: val
+				})
+
+				$(this).prop('value', val)
+			})
+		})
+	}
+
+	$('.filter__top').on('click', function() {
+		$(this).toggleClass('active')
+		$(this).next('.filter__body').toggleClass('active')
+	})
+
+	$('.filter__mobile').on('click', function() {
+		$('.filter').addClass('active')
+	})
+
+	$('.filter__head--close').on('click', function() {
+		$('.filter').removeClass('active')
+	})
+
+	$('.listing__hit a').on('click', function() {
+		$(this).addClass('active')
+		$(this).next().addClass('active')
+	})
+
+	$('.listing__hit--close').on('click', function() {
+		$(this).removeClass('active')
+		$(this).prev().removeClass('active')
+	})
+
+	$('.card__srav').on('click', function() {
+		$(this).toggleClass('active')
+	})
+
+	$('.card__like').on('click', function() {
+		$(this).toggleClass('active')
+
+		return false
+	})
+
+	$('.card-popup__like').on('click', function() {
+		$(this).toggleClass('active')
+
+		return false
+	})
+
+	$('.card-popup__srav').on('click', function() {
+		$(this).toggleClass('active')
+
+		return false
+	})
+
+	$('.listing__sort--info').on('click', function() {
+		$('.listing__sort--dropdown').toggleClass('active')
+	})
+
+	$('.listing__sort--item').on('click', function() {
+		$('.listing__sort--dropdown').removeClass('active')
+	})
+
+	$('[data-card-popup-tab]').on('click', function() {
+		if (!$(this).hasClass('active')) {
+			var index = $(this).index()
+			$(this).addClass('active').siblings().removeClass('active')
+			$('[data-card-popup-item]')
+				.removeClass('active')
+				.eq(index)
+				.addClass('active')
+		}
+		return false
+	})
+
+	var sliderThumbnail = new Swiper('.detal__container', {
+		slidesPerView: 7,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+		spaceBetween: 5
+	})
+
+	var slider = new Swiper('.detal__top', {
+		thumbs: {
+			swiper: sliderThumbnail
+		},
+
+		pagination: {
+			clickable: true,
+			el: '.swiper-pagination'
+		}
+	})
+
+	$('[data-detal-nav]').on('click', function() {
+		if (!$(this).hasClass('active')) {
+			var index = $(this).index()
+			$(this).addClass('active').siblings().removeClass('active')
+			$('[data-detal-item]').removeClass('active').eq(index).addClass('active')
+		}
+		return false
+	})
+
+	const $activeTab = $('.detal-tabs__top [data-detal-nav].active')
+
+	if ($activeTab.length) {
+		$('.detal-tabs__mobile span').text($activeTab.text())
+	}
+
+	$('.detal-tabs__mobile').on('click', function() {
+		$('.detal-tabs__top').toggleClass('active')
+		$(this).toggleClass('active')
+	})
+
+	$('[data-detal-nav]').on('click', function() {
+		$('[data-detal-nav]').removeClass('active')
+		$(this).addClass('active')
+
+		$('.detal-tabs__mobile span').text($(this).text())
+
+		$('.detal-tabs__top').removeClass('active')
+		$('.detal-tabs__mobile').removeClass('active')
+	})
+
+	const prodcom = new Swiper('.prod-com__slider', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+
+		navigation: {
+			prevEl: '.prod-com__prev.btn-swiper-prev',
+			nextEl: '.prod-com__next.btn-swiper-next'
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+		breakpoints: {
+			991: {
+				slidesPerView: 2
+			},
+			1200: {
+				slidesPerView: 3
+			}
+		}
+	})
+
+	$('.prod-rec').each(function() {
+		const $block = $(this)
+
+		new Swiper($block.find('.prod-rec__slider')[0], {
+			slidesPerView: 1,
+			spaceBetween: 20,
+
+			navigation: {
+				prevEl: $block.find('.prod-rec__prev.btn-swiper-prev')[0],
+				nextEl: $block.find('.prod-rec__next.btn-swiper-next')[0]
+			},
+
+			pagination: {
+				el: $block.find('.swiper-pagination')[0],
+				type: 'bullets',
+				clickable: true
+			},
+
+			breakpoints: {
+				991: {
+					slidesPerView: 2
+				},
+				1400: {
+					slidesPerView: 4
+				}
+			}
+		})
+	})
+
+	$('[data-search-tab]').on('click', function() {
+		if (!$(this).hasClass('active')) {
+			var index = $(this).index()
+			$(this).addClass('active').siblings().removeClass('active')
+			$('[data-search-item]').removeClass('active').eq(index).addClass('active')
+		}
+		return false
+	})
+
+	const aboutHistory = new Swiper('.about-history__container', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+
+		navigation: {
+			prevEl: '.about-history__prev.btn-swiper-prev',
+			nextEl: '.about-history__next.btn-swiper-next'
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+
+		loop: true,
+
+		breakpoints: {
+			991: {
+				slidesPerView: 'auto'
+			}
+		},
+		on: {
+			slideChangeTransitionEnd: function() {
+				const activeSlide = this.slides[this.activeIndex]
+				this.slides.forEach(slide => slide.classList.remove('is-active'))
+				if (activeSlide) activeSlide.classList.add('is-active')
+			}
+		}
+	})
+
+	const aboutNews = new Swiper('.about-news__container', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+
+		navigation: {
+			prevEl: '.about-news__prev.btn-swiper-prev',
+			nextEl: '.about-news__next.btn-swiper-next'
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+
+		breakpoints: {
+			991: {
+				slidesPerView: 2
+			}
+		}
+	})
+
+	$('.garant__head').on('click', function() {
+		$(this).toggleClass('active')
+		$(this).next().toggleClass('active')
+	})
+
+	$('.garant__more').on('click', function() {
+		const btn = $(this)
+		const text = btn.prev('.garant__text')
+
+		text.toggleClass('is-open')
+		btn.toggleClass('is-open')
+
+		btn
+			.find('span')
+			.text(text.hasClass('is-open') ? 'Свернуть' : 'Показать весь текст')
+	})
+
+	const decSlider = new Swiper('.dec-slider__content', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+
+		breakpoints: {
+			991: {
+				slidesPerView: 4
+			}
+		}
+	})
+
+	$('.field-file input[type="file"]').on('change', function() {
+		const $field = $(this).closest('.field-file')
+		const $info = $field.find('.field-file__info')
+
+		if (!this.files.length) return
+
+		$field.find('.field-file__selected').remove()
+
+		const fileName = this.files[0].name
+
+		const $fileBlock = $(`
+    <div class="field-file__selected">
+      <span class="field-file__name">${fileName}</span>
+      <span class="field-file__remove">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+  <path d="M12 12L4 4M12 4L4 12" stroke="#525558" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</span>
+    </div>
+  `)
+
+		$info.after($fileBlock)
+
+		$fileBlock.find('.field-file__remove').on('click', () => {
+			this.value = ''
+			$fileBlock.remove()
+		})
+	})
+
+	$('.price-block__head ').on('click', function() {
+		$(this).toggleClass('active')
+		$(this).next().toggleClass('active')
+	})
+
+	$('.price-block__grid--mobile').on('click', function() {
+		$(this).toggleClass('active')
+		$(this).next().toggleClass('active')
+	})
+
+	var init2 = false
+	var swiper2
+	function swiperTable() {
+		if (window.innerWidth < 991) {
+			if (!init2) {
+				init2 = true
+				swiper2 = new Swiper('.price-table__container', {
+					slidesPerView: 1,
+					spaceBetween: 10,
+					loop: true,
+					pagination: {
+						el: '.swiper-pagination',
+						type: 'bullets',
+						clickable: true
+					},
+
+					breakpoints: {
+						992: {
+							slidesPerView: 3
+						}
+					}
+				})
+			}
+		} else if (init2) {
+			swiper2.destroy()
+			init2 = false
+		}
+	}
+
+	swiperTable()
+	window.addEventListener('resize', swiperTable)
+})
+
+
+});
